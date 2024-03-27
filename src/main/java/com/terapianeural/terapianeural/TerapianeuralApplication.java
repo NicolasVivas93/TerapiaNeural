fecb40d28abf7f792a0ec8e8e5309a5885f1163c
NicolasVivas93 <nicolasvivas93@gmail.com>
2024-03-27 00:07:12 -0300

    Add project for terapia neural

---
+++ src/main/java/com/terapianeural/terapianeural/TerapianeuralApplication.java
@@ -0,0 +1,13 @@
+package com.terapianeural.terapianeural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerapianeuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerapianeuralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terapianeural/terapianeural/TerapianeuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terapianeural.terapianeural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerapianeuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
